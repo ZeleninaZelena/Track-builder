7a32d1dc32b32908fe90109bac36337d17bb291a
ZeleninaZelena <petr.pecho@seznam.cz>
2026-03-05 18:11:43 +0100

    Functioning data save/load

--- script.js
+++ script.js
@@ -7,26 +7,71 @@ const mainMenu = document.getElementById("main-menu");
 const editor = document.getElementById("editor");
 const mapGrid = document.getElementById("mapGrid");
 const savedMapList = document.getElementById("savedMapList");
-const mapName = document.getElementById("mapName");
-const importFile = document.getElementById("ImportFile");
+const mapNameInput = document.getElementById("mapName");
+const importFile = document.getElementById("importFile");
 
 const newMapButton = document.getElementById("newMapButton");
 newMapButton.addEventListener("click", () => {
     generateEmptyMap();
     sectionChange("editor");
-})
+});
 
 const loadMapButtonMenu = document.getElementById("loadMapButtonMenu");
 loadMapButtonMenu.addEventListener("click", () => {
     renderMapList();
-})
+});
 
 
 const mainMenuButton = document.getElementById("mainMenuButton");
 mainMenuButton.addEventListener("click", () => {
     sectionChange("main-menu")
-})
+});
 
+const saveMapButton = document.getElementById("saveMapButton");
+saveMapButton.addEventListener("click", () => {
+    const name = mapNameInput.value;
+    if (!name) {
+        return alert("Zadejte název mapy");
+    };
+    localStorage.setItem(name, JSON.stringify(mapData));
+    alert("Mapa uložena");
+
+});
+
+const exportMapButton = document.getElementById("exportMapButton");
+exportMapButton.addEventListener("click", () => {
+    const data = JSON.stringify(mapData);
+    const blob = new Blob([data], {type: "application/json"});
+    const downloadURL = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = downloadURL;
+    a.download = "map.json";
+    a.click();
+    URL.revokeObjectURL(downloadURL);
+
+});
+
+
+const importMapButton = document.getElementById("importMapButton")
+
+importMapButton.addEventListener("click", () => importFile.click());
+
+importFile.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = function(event) {
+        try {
+            mapData = JSON.parse(event.target.result);
+            drawMap();
+            sectionChange("editor");
+        } catch {
+            alert("Chybný soubor");
+        }
+    }
+    reader.readAsText(file);
+});
 
 function sectionChange(section) {
     mainMenu.classList.remove("hidden");
@@ -59,8 +104,8 @@ function drawMap() {
     for (let y = 0; y < mapSize; y++){
         for (let x = 0; x < mapSize; x++) {
             const gridTile = document.createElement("div");
-            gridTile.classList.add("tile");
-            gridTile.classList.add(tileTypes[0]);
+            gridTile.classList.add("tile", tileTypes[mapData[y][x]]);
+            
             gridTile.addEventListener("click", () => {
                 mapData[y][x] = (mapData[y][x] + 1) % tileTypes.length;
                 gridTile.className = "tile " + tileTypes[mapData[y][x]];
@@ -85,7 +130,7 @@ function renderMapList() {
         loadButton.addEventListener("click", () => {
             mapData = JSON.parse(localStorage.getItem(key));
             drawMap();
-            sectionChange(editor);
+            sectionChange("editor");
 
         })
         tmpButtons.appendChild(loadButton);
